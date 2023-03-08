27e6addf9e7168ecf3421d43266dac17d8bc7f2a
berathubb <beratates394@gmail.com>
2023-03-08 11:07:10 +0300

    Java Instagram Klonu
    İnstagram klonu kodları

---
+++ İnstagram-Klonu/app/src/main/java/com/beratates/instagramklonu/adapter/PostAdapter.java
@@ -0,0 +1,51 @@
+package com.beratates.instagramklonu.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.beratates.instagramklonu.databinding.RecylerRowBinding;
+import com.beratates.instagramklonu.model.Post;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostHolder> {
+    private ArrayList<Post> postArrayList;
+
+    public PostAdapter(ArrayList<Post> postArrayList) {
+        this.postArrayList = postArrayList;
+    }
+
+    @NonNull
+    @Override
+    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecylerRowBinding recylerRowBinding = RecylerRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent, false);
+        return  new PostHolder(recylerRowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+        holder.recylerRowBinding.recyclerViewUserEmailText.setText(postArrayList.get(position).email);
+        holder.recylerRowBinding.recyclerViewCommentText.setText(postArrayList.get(position).comment);
+        Picasso.get().load(postArrayList.get(position).downloadUrl).into(holder.recylerRowBinding.recyclerViewImageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return postArrayList.size();
+    }
+
+    class PostHolder extends RecyclerView.ViewHolder{
+        RecylerRowBinding recylerRowBinding;
+
+        public PostHolder(RecylerRowBinding recylerRowBinding) {
+            super(recylerRowBinding.getRoot());
+            this.recylerRowBinding = recylerRowBinding;
+        }
+    }
+}
---
+++ İnstagram-Klonu/app/src/main/java/com/beratates/instagramklonu/view/FeedActivity.java
@@ -0,0 +1,121 @@
+package com.beratates.instagramklonu.view;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.beratates.instagramklonu.R;
+import com.beratates.instagramklonu.adapter.PostAdapter;
+import com.beratates.instagramklonu.databinding.ActivityFeedBinding;
+import com.beratates.instagramklonu.model.Post;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class FeedActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore firebaseFirestore;
+    ArrayList<Post> postArrayList;
+    private ActivityFeedBinding binding;
+    PostAdapter postAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityFeedBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        postArrayList = new ArrayList<>();
+
+        getData();
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        postAdapter = new PostAdapter(postArrayList);
+        binding.recyclerView.setAdapter(postAdapter);
+    }
+
+    private void  getData(){
+        firebaseFirestore.collection("posts").orderBy("date", Query.Direction.DESCENDING).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if(error != null){
+                    Toast.makeText(FeedActivity.this,error.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                }
+                if(value != null){
+
+                    for(DocumentSnapshot documentSnapshot: value.getDocuments()){
+
+                        Map<String, Object>data = documentSnapshot.getData();
+
+                        //casting
+                        String userEmail = (String) data.get("useremail");
+                        String comment = (String) data.get("comment");
+                        String downloadUrl = (String) data.get("downloadUrl");
+
+                        Post post = new Post(userEmail,comment,downloadUrl);
+                        postArrayList.add(post);
+
+                    }
+                    postAdapter.notifyDataSetChanged();
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add_post){
+            //Upload kısmına yönlendiririlir.
+            Intent intentToUpload = new Intent(FeedActivity.this, UploadActivity.class);
+            startActivity(intentToUpload);
+
+        }else if(item.getItemId() == R.id.signout){
+            //signout kısmına yönlendirirlir.
+            Toast.makeText(FeedActivity.this, "Çıkış Yapıldı", Toast.LENGTH_LONG).show();
+
+            auth.signOut();
+
+            Intent intentToMain = new Intent(FeedActivity.this, MainActivity.class);
+            startActivity(intentToMain);
+            finish();
+
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
